e1aaa11eeecd19ace4a02ae7b34b23d712005201
Jonathan Fontes <jonathan.alexey@icloud.com>
2022-07-19 08:32:26 +0100

    add comments to Stack && Scope

--- semantic/stack.go
+++ semantic/stack.go
@@ -1,17 +1,24 @@
 package semantic
 
+// Scope is a map structure that hold a identifier and if is ready
+// or not.
 type Scope map[string]bool
 
+// Stack will keep track frames of a program, where a specific Scope
+// are in stack
 type Stack []*Scope
 
+// IsEmpty check if stack is empty
 func (s *Stack) IsEmpty() bool {
 	return len(*s) == 0
 }
 
+// Push will push new Scope into stack, will create a new frame.
 func (s *Stack) Push(i *Scope) {
 	*s = append(*s, i)
 }
 
+// Pop will pop last Scope pushed into Stack
 func (s *Stack) Pop() (*Scope, bool) {
 	if s.IsEmpty() {
 		return nil, false
@@ -22,6 +29,7 @@ func (s *Stack) Pop() (*Scope, bool) {
 	return element, true
 }
 
+// Peek only will peek last Scope pushed into Stack
 func (s *Stack) Peek() (*Scope, bool) {
 	if s.IsEmpty() {
 		return nil, false
@@ -31,14 +39,17 @@ func (s *Stack) Peek() (*Scope, bool) {
 	return scope, true
 }
 
-func (s *Scope) Put(name string, ready bool) {
-	(*s)[name] = ready
-}
-
+// Get will get specified index of scope
 func (s *Stack) Get(index int) *Scope {
 	return (*s)[index]
 }
 
+// Size will get total size of stack
 func (s *Stack) Size() int {
 	return len(*s)
+}
+
+// Put will put a new variable into Scope
+func (s *Scope) Put(name string, ready bool) {
+	(*s)[name] = ready
 }
