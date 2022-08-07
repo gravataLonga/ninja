c713616ed7eebe541fc12293f4a859c3553bf545
Jonathan Fontes <jonathan.alexey@icloud.com>
2022-08-07 12:28:15 +0100

    add more operator logic

--- interpreter/infix.go
+++ interpreter/infix.go
@@ -64,6 +64,42 @@ func infixExpression(v *ast.InfixExpression, operator string, left object.Object
 			return object.NewErrorFormat("%s %s", err, v.Token)
 		}
 		return obj
+	case "==":
+		obj, err := infixEqualExpression(left, right)
+		if err != nil {
+			return object.NewErrorFormat("%s %s", err, v.Token)
+		}
+		return obj
+	case "!=":
+		obj, err := infixNotEqualExpression(left, right)
+		if err != nil {
+			return object.NewErrorFormat("%s %s", err, v.Token)
+		}
+		return obj
+	case "<":
+		obj, err := infixLessExpression(left, right)
+		if err != nil {
+			return object.NewErrorFormat("%s %s", err, v.Token)
+		}
+		return obj
+	case ">":
+		obj, err := infixGreaterExpression(left, right)
+		if err != nil {
+			return object.NewErrorFormat("%s %s", err, v.Token)
+		}
+		return obj
+	case "<=":
+		obj, err := infixLessOrEqualExpression(left, right)
+		if err != nil {
+			return object.NewErrorFormat("%s %s", err, v.Token)
+		}
+		return obj
+	case ">=":
+		obj, err := infixGreaterOrEqualExpression(left, right)
+		if err != nil {
+			return object.NewErrorFormat("%s %s", err, v.Token)
+		}
+		return obj
 	}
 	return object.NewErrorFormat("unknown operator: %s %s %s", left.Type(), operator, right.Type())
 }
@@ -86,7 +122,10 @@ func infixPlusExpression(left, right object.Object) (object.Object, error) {
 			v := float64(right.(*object.Integer).Value)
 			return &object.Float{Value: left.(*object.Float).Value + v}, nil
 		}
-
+	case object.STRING_OBJ:
+		if right.Type() == object.STRING_OBJ {
+			return &object.String{Value: left.(*object.String).Value + right.(*object.String).Value}, nil
+		}
 	}
 	return nil, errors.New(fmt.Sprintf("unknown operator: %s %s %s", left.Type(), "+", right.Type()))
 }
@@ -224,4 +263,286 @@ func infixXorExpression(left, right object.Object) (object.Object, error) {
 	}
 
 	return &object.Integer{Value: left.(*object.Integer).Value ^ right.(*object.Integer).Value}, nil
+}
+
+func infixEqualExpression(left, right object.Object) (object.Object, error) {
+	switch left.Type() {
+	case object.INTEGER_OBJ:
+		switch right.Type() {
+		case object.INTEGER_OBJ:
+			if left.(*object.Integer).Value == right.(*object.Integer).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		case object.FLOAT_OBJ:
+			left := float64(left.(*object.Integer).Value)
+			if left == right.(*object.Float).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		default:
+			return object.FALSE, nil
+		}
+	case object.FLOAT_OBJ:
+		switch right.Type() {
+		case object.FLOAT_OBJ:
+			if left.(*object.Float).Value == right.(*object.Float).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		case object.INTEGER_OBJ:
+			right := float64(right.(*object.Integer).Value)
+			if left.(*object.Float).Value == right {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		default:
+			return object.FALSE, nil
+		}
+	case object.STRING_OBJ:
+		switch right.Type() {
+		case object.STRING_OBJ:
+			if left.(*object.String).Value == right.(*object.String).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		default:
+			return object.FALSE, nil
+		}
+	case object.BOOLEAN_OBJ:
+		switch right.Type() {
+		case object.BOOLEAN_OBJ:
+			if left.(*object.Boolean).Value == right.(*object.Boolean).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		default:
+			return object.FALSE, nil
+		}
+	case object.ARRAY_OBJ:
+		fallthrough
+	case object.HASH_OBJ:
+		return object.FALSE, nil
+	}
+	return nil, errors.New(fmt.Sprintf("unknown operator: %s %s %s", left.Type(), "==", right.Type()))
+}
+
+func infixNotEqualExpression(left, right object.Object) (object.Object, error) {
+	switch left.Type() {
+	case object.STRING_OBJ:
+		switch right.Type() {
+		case object.STRING_OBJ:
+			if left.(*object.String).Value != right.(*object.String).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		default:
+			return object.TRUE, nil
+		}
+	case object.INTEGER_OBJ:
+		switch right.Type() {
+		case object.INTEGER_OBJ:
+			if left.(*object.Integer).Value != right.(*object.Integer).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		case object.FLOAT_OBJ:
+			left := float64(left.(*object.Integer).Value)
+			if left != right.(*object.Float).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		default:
+			return object.TRUE, nil
+		}
+	case object.FLOAT_OBJ:
+		switch right.Type() {
+		case object.FLOAT_OBJ:
+			if left.(*object.Float).Value != right.(*object.Float).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		case object.INTEGER_OBJ:
+			right := float64(right.(*object.Integer).Value)
+			if left.(*object.Float).Value != right {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		default:
+			return object.TRUE, nil
+
+		}
+	case object.BOOLEAN_OBJ:
+		switch right.Type() {
+		case object.BOOLEAN_OBJ:
+			if left.(*object.Boolean).Value != right.(*object.Boolean).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		default:
+			return object.TRUE, nil
+		}
+	case object.HASH_OBJ:
+		fallthrough
+	case object.ARRAY_OBJ:
+		return object.TRUE, nil
+
+	}
+	return nil, errors.New(fmt.Sprintf("unknown operator: %s %s %s", left.Type(), "!=", right.Type()))
+}
+
+func infixLessExpression(left, right object.Object) (object.Object, error) {
+	if !object.IsNumber(left) || !object.IsNumber(right) {
+		return nil, errors.New(fmt.Sprintf("unknown operator: %s %s %s", left.Type(), "<", right.Type()))
+	}
+
+	switch left.Type() {
+	case object.INTEGER_OBJ:
+		switch right.Type() {
+		case object.INTEGER_OBJ:
+			if left.(*object.Integer).Value < right.(*object.Integer).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		case object.FLOAT_OBJ:
+			left := float64(left.(*object.Integer).Value)
+			if left < right.(*object.Float).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		}
+	case object.FLOAT_OBJ:
+		switch right.Type() {
+		case object.FLOAT_OBJ:
+			if left.(*object.Float).Value < right.(*object.Float).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		case object.INTEGER_OBJ:
+			right := float64(right.(*object.Integer).Value)
+			if left.(*object.Float).Value < right {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		}
+	}
+
+	return nil, errors.New(fmt.Sprintf("unknown operator: %s %s %s", left.Type(), "<", right.Type()))
+}
+
+func infixGreaterExpression(left, right object.Object) (object.Object, error) {
+	if !object.IsNumber(left) || !object.IsNumber(right) {
+		return nil, errors.New(fmt.Sprintf("unknown operator: %s %s %s", left.Type(), ">", right.Type()))
+	}
+
+	switch left.Type() {
+	case object.INTEGER_OBJ:
+		switch right.Type() {
+		case object.INTEGER_OBJ:
+			if left.(*object.Integer).Value > right.(*object.Integer).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		case object.FLOAT_OBJ:
+			left := float64(left.(*object.Integer).Value)
+			if left > right.(*object.Float).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		}
+	case object.FLOAT_OBJ:
+		switch right.Type() {
+		case object.FLOAT_OBJ:
+			if left.(*object.Float).Value > right.(*object.Float).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		case object.INTEGER_OBJ:
+			right := float64(right.(*object.Integer).Value)
+			if left.(*object.Float).Value > right {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		}
+	}
+
+	return nil, errors.New(fmt.Sprintf("unknown operator: %s %s %s", left.Type(), ">", right.Type()))
+}
+
+func infixLessOrEqualExpression(left, right object.Object) (object.Object, error) {
+	if !object.IsNumber(left) || !object.IsNumber(right) {
+		return nil, errors.New(fmt.Sprintf("unknown operator: %s %s %s", left.Type(), "<=", right.Type()))
+	}
+
+	switch left.Type() {
+	case object.INTEGER_OBJ:
+		switch right.Type() {
+		case object.INTEGER_OBJ:
+			if left.(*object.Integer).Value <= right.(*object.Integer).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		case object.FLOAT_OBJ:
+			left := float64(left.(*object.Integer).Value)
+			if left <= right.(*object.Float).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		}
+	case object.FLOAT_OBJ:
+		switch right.Type() {
+		case object.FLOAT_OBJ:
+			if left.(*object.Float).Value <= right.(*object.Float).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		case object.INTEGER_OBJ:
+			right := float64(right.(*object.Integer).Value)
+			if left.(*object.Float).Value <= right {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		}
+	}
+
+	return nil, errors.New(fmt.Sprintf("unknown operator: %s %s %s", left.Type(), "<=", right.Type()))
+}
+
+func infixGreaterOrEqualExpression(left, right object.Object) (object.Object, error) {
+	if !object.IsNumber(left) || !object.IsNumber(right) {
+		return nil, errors.New(fmt.Sprintf("unknown operator: %s %s %s", left.Type(), ">=", right.Type()))
+	}
+
+	switch left.Type() {
+	case object.INTEGER_OBJ:
+		switch right.Type() {
+		case object.INTEGER_OBJ:
+			if left.(*object.Integer).Value >= right.(*object.Integer).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		case object.FLOAT_OBJ:
+			left := float64(left.(*object.Integer).Value)
+			if left >= right.(*object.Float).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		}
+	case object.FLOAT_OBJ:
+		switch right.Type() {
+		case object.FLOAT_OBJ:
+			if left.(*object.Float).Value >= right.(*object.Float).Value {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		case object.INTEGER_OBJ:
+			right := float64(right.(*object.Integer).Value)
+			if left.(*object.Float).Value >= right {
+				return object.TRUE, nil
+			}
+			return object.FALSE, nil
+		}
+	}
+
+	return nil, errors.New(fmt.Sprintf("unknown operator: %s %s %s", left.Type(), ">", right.Type()))
 }
---
+++ interpreter/infix_test.go
@@ -0,0 +1,546 @@
+package interpreter
+
+import (
+	"fmt"
+	"github.com/gravataLonga/ninja/object"
+	"testing"
+)
+
+func TestInfixMathOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{
+			`1 + 1`,
+			2,
+		},
+		{
+			`1.0 + 1.0`,
+			2.0,
+		},
+		{
+			`1.0 + 1`,
+			2.0,
+		},
+		{
+			`1 + 1.0`,
+			2.0,
+		},
+		{
+			`"hello" + " ninja"`,
+			"hello ninja",
+		},
+		{
+			`1 - 1`,
+			0,
+		},
+		{
+			`1.0 - 1.0`,
+			0.0,
+		},
+		{
+			`1 - 1.0`,
+			0.0,
+		},
+		{
+			`1.0 - 1`,
+			0.0,
+		},
+		{
+			`2 * 2`,
+			4,
+		},
+		{
+			`2.0 * 2.0`,
+			4.0,
+		},
+		{
+			`2.0 * 2`,
+			4.0,
+		},
+		{
+			`2 * 2.0`,
+			4.0,
+		},
+		{
+			`2 / 2`,
+			1,
+		},
+		{
+			`2.0 / 2.0`,
+			1.0,
+		},
+		{
+			`2.0 / 2`,
+			1.0,
+		},
+		{
+			`2 / 2.0`,
+			1.0,
+		},
+		{
+			`4 % 2`,
+			0,
+		},
+		{
+			`4.0 % 2.0`,
+			0.0,
+		},
+		{
+			`4 % 2.0`,
+			0.0,
+		},
+		{
+			`4.0 % 2`,
+			0.0,
+		},
+		{
+			`10 ** 0`,
+			1,
+		},
+		{
+			`10.0 ** 0.0`,
+			1.0,
+		},
+		{
+			`10.0 ** 0`,
+			1.0,
+		},
+		{
+			`10 ** 0.0`,
+			1.0,
+		},
+		{
+			`1 | 0`,
+			1,
+		},
+		{
+			`1 & 0`,
+			0,
+		},
+		{
+			`1 ^ 0`,
+			1,
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("TestInfixOperator[%d]", i), func(t *testing.T) {
+
+			v := interpreter(t, tt.input)
+
+			if v == nil {
+				t.Fatalf("Interpreter return nil as result")
+			}
+
+			if _, ok := v.(*object.Error); ok {
+				t.Fatalf("Interpreter return error. %s", v.Inspect())
+			}
+
+			if !testLiteralObject(t, v, tt.expected) {
+				t.Fatalf("testLiteralObject got false, expected true.")
+			}
+		})
+	}
+}
+
+func TestInfixLogicOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{
+			`1 == 1`,
+			true,
+		},
+		{
+			`0 == 1`,
+			false,
+		},
+		{
+			`1 == 0`,
+			false,
+		},
+		{
+			`1 == 0`,
+			false,
+		},
+		{
+			`1.0 == 1.0`,
+			true,
+		},
+		{
+			`1.0 == 0.0`,
+			false,
+		},
+		{
+			`0.0 == 1.0`,
+			false,
+		},
+		{
+			`1 == 1.0`,
+			true,
+		},
+		{
+			`1.0 == 1`,
+			true,
+		},
+		{
+			`1.0 == 1`,
+			true,
+		},
+		{
+			`true == true`,
+			true,
+		},
+		{
+			`true == false`,
+			false,
+		},
+		{
+			`true == "ninja"`,
+			false,
+		},
+		{
+			`true == 1`,
+			false,
+		},
+		{
+			`true == 1.0`,
+			false,
+		},
+		{
+			`"ninja" == true`,
+			false,
+		},
+		{
+			`1 == true`,
+			false,
+		},
+		{
+			`1.0 == true`,
+			false,
+		},
+		{
+			`"ninja" == "ninja"`,
+			true,
+		},
+		{
+			`"ninja" == "hello"`,
+			false,
+		},
+		{
+			`"ninja" == 1`,
+			false,
+		},
+		{
+			`1 == "ninja"`,
+			false,
+		},
+		{
+			`1.0 == "ninja"`,
+			false,
+		},
+		{
+			`"ninja" == 1.0`,
+			false,
+		},
+		{
+			`[] == []`,
+			false,
+		},
+		{
+			`{} == {}`,
+			false,
+		},
+		{
+			`1 != 1`,
+			false,
+		},
+		{
+			`0 != 1`,
+			true,
+		},
+		{
+			`1.0 != 1.0`,
+			false,
+		},
+		{
+			`1 != 1.0`,
+			false,
+		},
+		{
+			`1.0 != 1`,
+			false,
+		},
+		{
+			`"ninja" != "wow"`,
+			true,
+		},
+		{
+			`"ninja" != "ninja"`,
+			false,
+		},
+		{
+			`"ninja" != "ninja"`,
+			false,
+		},
+		{
+			`"ninja" != 1`,
+			true,
+		},
+		{
+			`1 != "ninja"`,
+			true,
+		},
+		{
+			`1.0 != "ninja"`,
+			true,
+		},
+		{
+			`"ninja" != 1.0`,
+			true,
+		},
+		{
+			`"ninja" != []`,
+			true,
+		},
+		{
+			`[] != "ninja"`,
+			true,
+		},
+		{
+			`"ninja" != {}`,
+			true,
+		},
+		{
+			`{} != "ninja"`,
+			true,
+		},
+		{
+			`1 != []`,
+			true,
+		},
+		{
+			`[] != 1`,
+			true,
+		},
+		{
+			`1 != {}`,
+			true,
+		},
+		{
+			`{} != 1`,
+			true,
+		},
+		{
+			`1.0 != []`,
+			true,
+		},
+		{
+			`[] != 1.0`,
+			true,
+		},
+		{
+			`1.0 != {}`,
+			true,
+		},
+		{
+			`{} != 1.0`,
+			true,
+		},
+		{
+			`true != true`,
+			false,
+		},
+		{
+			`false != true`,
+			true,
+		},
+		{
+			`true != 1`,
+			true,
+		},
+		{
+			`true != 1.0`,
+			true,
+		},
+		{
+			`true != []`,
+			true,
+		},
+		{
+			`true != {}`,
+			true,
+		},
+
+		{
+			`1 != true`,
+			true,
+		},
+		{
+			`1.0 != true`,
+			true,
+		},
+		{
+			`[] != true`,
+			true,
+		},
+		{
+			`{} != true`,
+			true,
+		},
+
+		{
+			`1 < 1`,
+			false,
+		},
+		{
+			`0 < 1`,
+			true,
+		},
+		{
+			`1.0 < 1.0`,
+			false,
+		},
+		{
+			`0.0 < 1.0`,
+			true,
+		},
+		{
+			`1 < 1.0`,
+			false,
+		},
+		{
+			`0 < 1.0`,
+			true,
+		},
+		{
+			`1.0 < 1`,
+			false,
+		},
+		{
+			`1.0 < 0`,
+			false,
+		},
+
+		{
+			`1 > 1`,
+			false,
+		},
+		{
+			`0 > 1`,
+			false,
+		},
+		{
+			`1.0 > 1.0`,
+			false,
+		},
+		{
+			`0.0 > 1.0`,
+			false,
+		},
+		{
+			`1 > 1.0`,
+			false,
+		},
+		{
+			`0 > 1.0`,
+			false,
+		},
+		{
+			`1.0 > 1`,
+			false,
+		},
+		{
+			`1.0 > 0`,
+			true,
+		},
+
+		{
+			`1 <= 1`,
+			true,
+		},
+		{
+			`0 <= 1`,
+			true,
+		},
+		{
+			`1.0 <= 1.0`,
+			true,
+		},
+		{
+			`0.0 <= 1.0`,
+			true,
+		},
+		{
+			`1 <= 1.0`,
+			true,
+		},
+		{
+			`0 <= 1.0`,
+			true,
+		},
+		{
+			`1.0 <= 1`,
+			true,
+		},
+		{
+			`1.0 <= 0`,
+			false,
+		},
+
+		{
+			`1 >= 1`,
+			true,
+		},
+		{
+			`0 >= 1`,
+			false,
+		},
+		{
+			`1.0 >= 1.0`,
+			true,
+		},
+		{
+			`0.0 >= 1.0`,
+			false,
+		},
+		{
+			`1 >= 1.0`,
+			true,
+		},
+		{
+			`0 >= 1.0`,
+			false,
+		},
+		{
+			`1.0 >= 1`,
+			true,
+		},
+		{
+			`1.0 >= 0`,
+			true,
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("TestInfixOperator[%d]", i), func(t *testing.T) {
+
+			v := interpreter(t, tt.input)
+
+			if v == nil {
+				t.Fatalf("Interpreter return nil as result")
+			}
+
+			if _, ok := v.(*object.Error); ok {
+				t.Fatalf("Interpreter return error. %s", v.Inspect())
+			}
+
+			if !testLiteralObject(t, v, tt.expected) {
+				t.Fatalf("testLiteralObject got false, expected true.")
+			}
+		})
+	}
+}
